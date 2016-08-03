90007af3209227f2a2c9f372f90b7aa248717ae5
Matt Snoby <snobym@cisco.com>
2016-08-03 00:37:39 -0400

    changed the commands so help would show how to better use the program

--- main.go
+++ main.go
@@ -7,35 +7,33 @@ import (
 )
 
 var (
-	app     = kingpin.New("mkv2Appletv", "Convert as efficiently as possible media to AppleTV mp4 format.")
-	debug   = app.Flag("debug", "Enable debug mode.").Bool()
-	input   = app.Flag("input", "Location of input File").Short('i').Required().ExistingFile()
-	outFile = app.Flag("output", "Name of the output mp4 file, this is optional").Short('o').String()
+	app   = kingpin.New("mkv2Appletv", "Convert as efficiently as possible media to AppleTV mp4 format.")
+	debug = app.Flag("debug", "Enable debug mode.").Bool()
 
-	show = app.Command("show", "Using ffprobe show relavant information about a input file").Default()
-	//inputFile = show.Arg("input", "Name of Input File passed to ffprobe ").Required().ExistingFile()
+	show      = app.Command("show", "Using ffprobe show relavant information about a input file")
+	showinput = show.Arg("input", "Location of input File").Required().ExistingFile()
 
-	convert = app.Command("convert", "convert the input file to the best possible output file")
-	//inFile  = convert.Arg("input", "Name of Input File used as the source for conversion to an mp4 ").Required().ExistingFile()
-
+	suggest      = app.Command("suggest", "Show what the suggested output of the transformation would look like.")
+	suggestinput = suggest.Arg("input", "Location of input File").Required().ExistingFile()
 )
 
 func main() {
 	var ()
 
-	kingpin.Version("0.0.1")
+	kingpin.Version("0.0.2")
 	app.UsageTemplate(kingpin.SeparateOptionalFlagsUsageTemplate)
+	//app.UsageTemplate(kingpin.DefaultUsageTemplate)
 
 	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
 
 	// Register user
 	case show.FullCommand():
-		println("Input file ", (*input))
-		showFFprobeInfo((*input))
+		println("Input file ", (*showinput))
+		showFFprobeInfo((*showinput))
 
-	case convert.FullCommand():
-		println("Input file ", (*input))
-		suggestConvSettings((*input))
+	case suggest.FullCommand():
+		println("Input file ", (*suggestinput))
+		suggestConvSettings((*suggestinput))
 
 	}
 }
