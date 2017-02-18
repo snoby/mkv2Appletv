26d31c9737ec09f1e7b5e9d41c9603c8e2ed0f76
Matt Snoby <snobym@cisco.com>
2017-02-18 00:19:19 -0500

    Fixed issue with libx64 video convert arguement

--- convert.go
+++ convert.go
@@ -21,19 +21,34 @@ type ffmpegOut struct {
 }
 
 func (buff *ffmpegOut) genVideoConversion() error {
+	ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-map")
 
-	temp := fmt.Sprintf(" -map 0:%d -c:v", media.masterVideoStream.Index)
+	temp := fmt.Sprintf("0:%d", media.masterVideoStream.Index)
+	ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, temp)
+	ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-c:v")
+
 	switch media.outVideo {
 
 	case "copy":
-		buff.Video = fmt.Sprintf(" copy")
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "copy")
 	case "convert":
-		buff.Video = fmt.Sprintf("libx264 -preset slow -crf 20 -profile:v high -level 4.0 ")
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "libx264")
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-preset")
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "slow")
+
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-crf")
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "20")
+
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-profile:v")
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "high")
+
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-level")
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "4.0")
+
 	default:
 		err := errors.New("unknown or not set Video settings\n")
 		return err
 	} // end of switch
-	ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, temp, buff.Video)
 	return err
 }
 
@@ -47,7 +62,8 @@ func (buff *ffmpegOut) genAudioConversion() error {
 		buff.Audio0 = fmt.Sprintf("-c:a:0")
 		map1 := fmt.Sprintf("0:%d", media.masterAudioStream.Index)
 		buff.Audio1 = fmt.Sprintf("-c:a:1")
-		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, " -map", map0, buff.Audio0, "copy", "-map", map1, buff.Audio1, "copy")
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-map")
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, map0, buff.Audio0, "copy", "-map", map1, buff.Audio1, "copy")
 
 		return err
 	} else if media.outAudio0 == "convert" && media.outAudio1 == "convert" {
@@ -59,11 +75,12 @@ func (buff *ffmpegOut) genAudioConversion() error {
 			// the asplit filter takes the input and splits it into 2 dual streams.  one called 2ch and another called 6ch
 			// The 2ch is fed into the pan filter and the output is placed into the "aac" pad
 			//
-			buff.Audio0 = fmt.Sprintf("-filter_complex [0:%d]asplit[2ch][6ch];[2ch]pan=stereo|FL=FC+0.6FL+0.2BL|FR=FC+0.6FR+0.2BR[aac]", media.masterAudioStream.Index)
+			ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-filter_complex")
+			buff.Audio0 = fmt.Sprintf("[0:%d]asplit[2ch][6ch];[2ch]pan=stereo|FL=FC+0.6FL+0.2BL|FR=FC+0.6FR+0.2BR[aac]", media.masterAudioStream.Index)
 			ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, buff.Audio0)
 			// Now append the output pad mappings [aac] and [6ch]
 			// if the 6ch is NOT ac3 we will have to transcode it.
-			ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-map [aac]", "-map", "[6ch]", "-c:a:0", "aac", "-c:a:1", "ac3")
+			ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-map", "[aac]", "-map", "[6ch]", "-c:a:0", "aac", "-c:a:1", "ac3")
 		} else {
 			// if the master audio is NOT aac and is only 2 channel
 			buff.Audio0 = fmt.Sprintf("-map 0:%d -c:a:0 aac -b:a 256k", media.masterAudioStream.Index)
@@ -102,9 +119,9 @@ func (buff *ffmpegOut) genAudioConversion() error {
 }
 func (buff *ffmpegOut) setupHeader() {
 
-	ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-map_metadata 0:g")
+	ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-map_metadata", "0:g")
 	if *try == true {
-		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-t 00:00:10")
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-t", "00:00:10")
 	}
 
 }
@@ -116,10 +133,10 @@ func convertSource(in string, output string) {
 
 	// Do we need to handle additional debugging?
 	if *debug == true {
-		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-v trace")
+		ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-v", "debug")
 	}
-	temp := fmt.Sprintf(" -i %s", in)
-	ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-hide_banner", "-y", temp)
+	ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-i", in)
+	ffmpegCmd.ffArgs = append(ffmpegCmd.ffArgs, "-hide_banner", "-y")
 
 	if output != "" {
 		ffmpegCmd.outFile = output
